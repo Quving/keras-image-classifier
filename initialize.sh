0ec63484f4c6278a8da347a9b370000ae2765eaf
Vinh Ngu <vinh-ngu@hotmail.com>
2018-02-14 13:19:52 +0100

    Create folder initially for from-scratch approach.

--- initialize.sh
+++ initialize.sh
@@ -5,3 +5,10 @@ mkdir -p use-vgg16/bottleneck_features
 mkdir -p use-vgg16/models
 mkdir -p use-vgg16/class_indices
 
+
+mkdir -p from-scratch/training
+mkdir -p from-scratch/bottleneck_features
+mkdir -p from-scratch/models
+mkdir -p from-scratch/class_indices
+
+
